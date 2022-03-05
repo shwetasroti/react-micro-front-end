d3bce2296e48cd0ba1240377851dff8705887f89
sshweta7 <spsucbooth7@gmail.com>
2022-03-05 22:16:21 +0530

    [ss] react-micro-front end

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # react-micro-front-end
 Micro Front-end application using React, Webpack 5, and Module Federation
+Inside mfe1 and mfe2 do these steps:
+1. npm install
+2. yarn webpack serve
+3. open browser on localhost:8082 
---
+++ mf2e/src/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+const MFE1_Button = React.lazy(
+  () => import('MFE1/Button')
+);
+
+function App() {
+  return (
+    <div>
+      <h1>
+        Micro-Frontend Host
+      </h1>
+
+      <div
+        style={{
+          margin: '10px',
+          padding: '10px',
+          width: '60%',
+          border:
+            '4px solid black',
+          background: '#f8e4c5', 
+        }}>
+        <h3>
+          Button from MFE1
+        </h3>
+        <React.Suspense fallback='Loading Button'>
+          <MFE1_Button />
+        </React.Suspense>
+      </div>
+    </div>
+  );
+}
+
+export default App;
